2c7d37742fdbd1714e346991d94475132a718f0c
elyaDev <57408112+Fury6z@users.noreply.github.com>
2022-04-24 07:36:22 +0200

    Update settings.py

--- easysoftcrm_django/settings.py
+++ easysoftcrm_django/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'easysoftcrm_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'easysoftcrm_ppe',
+        'NAME': 'easysoftcrm-ppe',
         'USER': 'easysoftcrm_user',
         'PASSWORD': '%%@*9x5!w5sEzH5YfwyvaqVJC',
         'HOST': '127.0.0.1',
